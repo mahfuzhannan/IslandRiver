2066c6f9ed0b825a0392767cc0eb7d7889d49a04
Mahfuz Hannan <mahfu2@live.com>
2017-03-24 15:29:28 +0000

    rename app

--- IslandRiver/wsgi.py
+++ IslandRiver/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WebECommerce project.
+WSGI config for IslandRiver project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WebECommerce.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IslandRiver.settings")
 
 application = get_wsgi_application()
